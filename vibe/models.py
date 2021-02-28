b9e6eaaf07d7aec00585aa13b804dc3f2665d50e
titus njiru <njirutitus@gmail.com>
2021-02-28 20:07:49 +0300

    Adding Album and Song models

--- vibe/models.py
+++ vibe/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import Permission, User
 
-# Create your models here.
+class Album(models.Model):
+    user = models.ForeignKey(User,default=1,on_delete=models.CASCADE)
+    artist = models.CharField(max_length=255)
+    genre = models.CharField(max_length=500)
+    album_title = models.CharField(max_length=500)
+    album_log = models.FileField()
+    is_favorite = models.BooleanField(default=False)
+
+class Song(models.Model):
+    album = models.ForeignKey(Album,on_delete=models.CASCADE)
+    song_title = models.CharField(max_length=500)
+    audio_file = models.FileField()
+    is_favorite = models.BooleanField(default=False)
+    
+
